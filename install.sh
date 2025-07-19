c6565b144d846b80312d4db145fde408f1e6f911
Leonel Ramirez <95013328+FarikoM2@users.noreply.github.com>
2025-07-18 23:58:17 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,16 +7,15 @@ sudo apt update
 sudo apt install -y bspwm sxhkd rxvt-unicode rofi picom feh xorg xinit unzip
 
 # 2. Crear carpetas necesarias
-mkdir -p ~/.config/bspwm ~/.config/sxhkd ~/.config
+mkdir -p ~/.config/bspwm ~/.config/sxhkd ~/.config/picom
 mkdir -p ~/.fonts
 mkdir -p ~/wallpapers
 mkdir -p /tmp/grimm-fonts
-mkdir -p /tmp/grimm-fonts/unpacked
 
 # 3. Copiar configuraciones
 cp ./config/bspwmrc ~/.config/bspwm/bspwmrc
 cp ./config/sxhkdrc ~/.config/sxhkd/sxhkdrc
-cp ./config/picom.conf ~/.config/picom.conf
+cp ./config/picom.conf ~/.config/picom/picom.conf
 cp ./xinitrc ~/.xinitrc
 cp ./Xresources ~/.Xresources
 cp ./wallpapers/* ~/wallpapers/
@@ -25,21 +24,15 @@ cp ./wallpapers/* ~/wallpapers/
 echo "[+] Descargando fuentes..."
 while read -r url; do
     echo "Descargando: $url"
-    wget --content-disposition --trust-server-names -P /tmp/grimm-fonts "$url"
-
-    # Extraer el archivo más reciente descargado
-    last_zip=$(find /tmp/grimm-fonts -name "*.zip" -type f -printf "%T@ %p\n" | sort -n | tail -1 | cut -d' ' -f2-)
-
-    if file "$last_zip" | grep -q "Zip archive data"; then
-        unzip -qq "$last_zip" -d /tmp/grimm-fonts/unpacked
-    else
-        echo "[!] Error: $last_zip no es un archivo ZIP válido."
-    fi
+    wget -P /tmp/grimm-fonts "$url"
 done < ./fonts/fonts.list
 
+# Extraer todos los .zip descargados directamente en la misma carpeta
+unzip -qq '/tmp/grimm-fonts/*.zip' -d /tmp/grimm-fonts
+
 # 5. Mover las fuentes instaladas
-find /tmp/grimm-fonts/unpacked -iname "*.ttf" -exec cp {} ~/.fonts/ \;
-find /tmp/grimm-fonts/unpacked -iname "*.otf" -exec cp {} ~/.fonts/ \;
+find /tmp/grimm-fonts -iname "*.ttf" -exec cp {} ~/.fonts/ \;
+find /tmp/grimm-fonts -iname "*.otf" -exec cp {} ~/.fonts/ \;
 
 # 6. Limpiar archivos temporales
 rm -rf /tmp/grimm-fonts
